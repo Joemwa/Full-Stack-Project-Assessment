d1c8196aac851185ab3f22afffe90fe0a1b3ddfc
Joemwa <109852314+Joemwa@users.noreply.github.com>
2023-10-01 21:43:04 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   // const [enterTitle, setEnterTitle] = useState("");
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/")
+    fetch("https://fullstackbackend-92zs.onrender.com/")
       .then((res) => {
         return res.json();
       })
